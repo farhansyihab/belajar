00e3f89a8a1f72a8a3ceecddb77e0a3d203fcf56
Muhamad Farhan <agiptek@gmail.com>
2021-08-20 17:04:42 +0700

    tambah class untuk membuat object

--- javascript/createobject/index.js
+++ javascript/createobject/index.js
@@ -46,3 +46,31 @@ function Manusia (nama, umur, energi) {
 const Haidar = new Manusia('Haidar', 8, 12);
 Haidar.makan(22);
 Haidar.main(3);
+
+/** 4. Menggunakan Class */
+class Pelajar {
+    constructor(nama, umur, energi){
+        this.nama = nama;
+        this.umur = umur;
+        this.energi = energi;
+    }
+
+    makan = function (porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, energi Anda sekarang ${this.energi}`)
+    }
+
+    main = function(jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, energi Anda sekarang ${this.energi}`)
+    }
+
+    tidur = (jam) => {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, energi Anda sekarang ${this.energi}`)
+    }
+}
+
+const Sukainah = new Pelajar('Sukainah', 8, 10);
+Sukainah.makan(22);
+Sukainah.main(3);
